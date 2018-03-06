8fd9b5b47d06979c199a24a052c419df5d1c4b8a
Anna Leach <anna,leach@theguardian.com>
2018-03-06 18:41:21 +0000

    save formstack data

--- applications/app/controllers/ApplicationsControllers.scala
+++ applications/app/controllers/ApplicationsControllers.scala
@@ -39,6 +39,7 @@ trait ApplicationsControllers {
   lazy val indexController = wire[IndexController]
   lazy val siteVerificationController = wire[SiteVerificationController]
   lazy val shareCountController = wire[ShareCountController]
+  lazy val formstackController = wire[FormstackController]
 
   //A fake geolocation controller to test it locally
   lazy val geolocationController = wire[FakeGeolocationController]
--- static/src/javascripts/projects/journalism/campaigns.js
+++ static/src/javascripts/projects/journalism/campaigns.js
@@ -1,35 +1,78 @@
 import config from 'lib/config';
 import fastdom from 'lib/fastdom-promise';
 
+const formstackPostPath = "/formstack-campaign/submit";
+
 export function init (){
+  console.log("something happened");
+  
+  // extract data 
   const allCampaigns = config.get("page.campaigns");
-  
   const campaignToShow = allCampaigns.filter(isTip).pop();
-  const campaignFields = allCampaigns["fields"];
+  const campaignFields = campaignToShow["fields"];
+  const formFields = campaignFields["formFields"];
   
-  const articleBody = document.querySelector(".content__article-body");
+  // create container, title & description 
+  const campaignElement = document.createElement("details");
+  const summaryElement = document.createElement("summary");
+  const titleEl = document.createElement("p");
+  const descripEl = document.createElement("p");  
+  titleEl.textContent = campaignFields["callout"];
+  descripEl.textContent = campaignFields["description"];
   
-  // console.log("=====>", campaignToShow["fields"]["callout"]);
+  // create form 
+  const formEl = document.createElement("form");
+  formEl.setAttribute("method", "post");
+  formEl.setAttribute("action", formstackPostPath);
+  const submitButton = document.createElement("input");
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute("value", "Share with the Guardian!!!!");
   
-  const campaignElement = document.createElement("div");
-  const titleEl = document.createTextNode(campaignFields["callout"]);
-  const descripEl = document.createTextNode(campaignFields["description"]);
-  
-  const theBox = campaignElement.append(titleEl, descripEl);  
-  
-  // console.log("description", theBox);
-  
-  function isTip(campaign){
-    return campaign.fields._type === "tip"
-  }
-  
-  return fastdom.write(() => { 
-    return articleBody.appendChild(<p>SOME ELEMENT</p>);
+  // create form fields 
+  const formFieldEls = formFields.map(field => {
+    const fieldset = document.createElement("fieldset");
+    const label = document.createElement("label");
+    const labelSpan = document.createElement("span");
+    const input = document.createElement("input");
     
+    label.textContent = field["label"];
+    labelSpan.textContent = field["description"];
+    label.setAttribute("for", field["name"]);
+    label.appendChild(labelSpan);    
+    
+    input.setAttribute("type", field["type"]);
+    input.setAttribute("name", field["name"]);
+    if(field["required"] === "1"){input.setAttribute("required", "true")};
+    
+    fieldset.appendChild(label);
+    fieldset.appendChild(input);
+    return fieldset;
   });
   
-  /*
-  1- query the dom for article body
-  2- build a dom tree
-  3- append dom tree to b body*/
-}; 
+  // append form elements to form 
+  formFieldEls.forEach(el => {
+    formEl.appendChild(el);
+  });
+  formEl.appendChild(submitButton);
+  
+  console.log(formEl);
+  
+  // append everything to container
+  summaryElement.appendChild(titleEl);
+  summaryElement.appendChild(descripEl);
+  campaignElement.appendChild(summaryElement);
+  campaignElement.appendChild(formEl);
+  
+  // select article 
+  const article = document.querySelector(".content__article-body");
+  const endOfArticle = document.querySelector(".content__article-body .submeta");
+
+  // write to DOM 
+  return fastdom.write(() => {
+    return article.insertBefore(campaignElement, endOfArticle);  
+  });
+
+  function isTip(campaign){
+    return campaign.fields._type === "tip";
+  }
+};
