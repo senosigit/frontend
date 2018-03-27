90a76063f759a1d6dcc89435d6bd45cac2d51cca
Anna Leach <anna,leach@theguardian.com>
2018-03-27 18:01:33 +0100

    Fix css

--- static/src/javascripts/projects/journalism/modules/render-campaign.js
+++ static/src/javascripts/projects/journalism/modules/render-campaign.js
@@ -3,30 +3,22 @@ import template from 'lodash/utilities/template';
 import campaignTemplate from 'raw-loader!journalism/views/campaigns.html';
 import { init as initCampaign } from './campaigns';
 
-
 export const renderCampaign = () => {
-    const article = document.querySelector('.content__article-body');
-    // const endOfArticle = document.querySelector('.content__article-body .submeta');
-    makeCampaign(article);
+    const endOfArticle = document.querySelector(
+        '.content__article-body .submeta'
+    );
+    makeCampaign(endOfArticle);
 };
 
-const makeCampaign = (campaignSlotNode) => {
+const makeCampaign = anchorNode => {
+    console.log('in rendereer', initCampaign());
 
-    console.log("this ran - make Campaign");
+    const campaign = template(campaignTemplate, { data: initCampaign() });
+    const campaignDiv = `<figure class="element element-campaign">${
+        campaign
+    }</figure>`;
 
-    console.log("in rendereer", initCampaign());
-
-    const campaign = template(campaignTemplate, {
-        data: initCampaign(),
+    fastdom.write(() => {
+        anchorNode.insertAdjacentHTML('beforebegin', campaignDiv);
     });
-
-    const campaignDiv = `<figure class="element element-campaign">${campaign}</figure>`;
-
-    console.log("campaign div", campaignDiv);
-
-    return fastdom.write(() => {
-        campaignSlotNode.insertAdjacentHTML('beforeend', campaignDiv);
-    });
-
-    return Promise.resolve(null);
 };
