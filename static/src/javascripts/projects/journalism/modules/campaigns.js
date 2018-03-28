91970bd7428ce9449052caebcb72f5e821e94109
Anna Leach <anna,leach@theguardian.com>
2018-03-28 17:40:19 +0100

    design amends

--- static/src/javascripts/projects/journalism/modules/campaigns.js
+++ static/src/javascripts/projects/journalism/modules/campaigns.js
@@ -4,15 +4,13 @@ import config from 'lib/config';
 export const init = () => {
     // eslint-disable-next-line
     const isCallout = campaign => campaign.fields._type === 'callout';
-
     const allCampaigns = config.get('page.campaigns');
+
     const campaignToShow = allCampaigns.filter(isCallout).pop();
 
-    const returnObj = {
+    return {
         title: campaignToShow.fields.callout,
         description: campaignToShow.fields.description,
         formFields: campaignToShow.fields.formFields,
     };
-
-    return returnObj;
 };
